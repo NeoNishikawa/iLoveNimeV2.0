7443225bdfca746af450bf7fdcbe7bb6427de1cf
NeoNishikawa <Nerogame2007@gmail.com>
2026-09-19 21:46:21 +0700

    Delete desktop directory

--- desktop/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("desktopApp", Object.freeze({
-  platform: process.platform,
-  isDesktop: true,
-  cursorMode: "three-dimensional",
-  toggleFullscreen: () => ipcRenderer.invoke("toggle-fullscreen"),
-}));
